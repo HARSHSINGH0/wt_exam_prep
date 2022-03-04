608084efd1eb918a5937f35ce26c62b7b1309918
HARSH SINGH <41187121+HARSHSINGH0@users.noreply.github.com>
2022-03-04 12:21:37 +0530

    Update primenumber.js

--- primenumber.js
+++ primenumber.js
@@ -1,10 +1,33 @@
-var f=require('./primenumber');
-var x=3;
-if(f.isprime(x)){
-    console.log(x+" is not prime number");
+module.exports.isprime=function (a) {
+    var i;
+    for(i=2;i<=(a/2);i++){
+        if(a%i==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
-else{
-    console.log(x+" is prime number")
+
+module.exports.reverse=function(x){
+    var s=x.toString();
+    var rev="";
+    for(var index=x.length-1;index>-1;index--){
+        rev+=x[index];
+
+    }
+    return(rev);
 }
-var myname="harsh";
-console.log("Reverse of "+myname+" is "+f.reverse(myname));
+
+module.exports.Student=function(firstName,lastName,sub1,sub2,sub3,sub4){
+    this.firstName=firstName,
+    this.lastName=lastName,
+    this.sub1=sub1,
+    this.sub2=sub2,
+    this.sub3=sub3,
+    this.sub4=sub4,
+    percentage=((this.sub1+this.sub2+this.sub3+this.sub4)/400)*100;
+    //return percentage;
+    this.displayPercentage=function(){return percentage;}
+}
