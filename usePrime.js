6f7aa78bc0feec03aa564ffdb79e9dabd04f9403
HARSH SINGH <41187121+HARSHSINGH0@users.noreply.github.com>
2022-03-04 12:21:51 +0530

    Update usePrime.js

--- usePrime.js
+++ usePrime.js
@@ -6,3 +6,14 @@ if(f.isprime(x)){
 else{
     console.log(x+" is prime number")
 }
+var myname="harsh";
+console.log("Reverse of "+myname+" is "+f.reverse(myname));
+var firstname="Harsh";
+var lastname="Singh";
+var sub1=10;
+var sub2=21;
+var sub3=87;
+var sub4=91;
+//console.log(f.Student(firstname,lastname,sub1,sub2,sub3,sub4));
+var student=new f.Student(firstname,lastname,sub1,sub2,sub3,sub4);
+console.log("Percentage scored is "+student.displayPercentage());
